0eccd775e14d726c81ecb729d441af34742f90cc
Kod-e <115715642+Kod-e@users.noreply.github.com>
2025-08-01 01:17:59 +0900

    add judge route

--- app/api/v1/endpoints/question.py
+++ app/api/v1/endpoints/question.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.services.agent.question.agent import QuestionAgent
 from app.services.question.types import QuestionUnion, QuestionAdapter
 from typing import List
+from app.services.logic.question import QuestionHandler
+from app.services.question.common.spec import JudgeResult
+from app.infra.context import uow_ctx
+
 router = APIRouter(prefix="/question", tags=["question"])
 
 @router.post("/agent/chat", response_model=List[QuestionUnion])
@@ -9,3 +13,9 @@ async def make_question_by_chat(user_input: str):
     question_agent = QuestionAgent()
     await question_agent.run(user_input)
     return QuestionAdapter.validate_python(question_agent.question_stack.questions)
+
+
+@router.post("/judge" , response_model=JudgeResult)
+async def judge_question(question: QuestionUnion):
+    question_handler = QuestionHandler()
+    return await question_handler.judge(question)
---
+++ app/services/logic/question.py
@@ -0,0 +1,19 @@
+from app.services.question.types import QuestionUnion
+from app.services.question.common.spec import QuestionSpec, JudgeResult
+from app.services.common.mistake import MistakeService
+from app.infra.context import uow_ctx
+
+class QuestionHandler:
+    def __init__(self):
+        self._uow = uow_ctx.get()
+        
+    
+    # 处理题目, 判断错误, 并且存入mistake表
+    async def judge(self, question: QuestionSpec, answer: str) -> JudgeResult:
+        # 判断错误
+        judge_result: JudgeResult = await question.judge(answer)
+        if not judge_result.correct:
+            # 存入mistake表
+            self._uow.db.add(question.to_mistake(judge_result))
+            pass
+        return judge_result
