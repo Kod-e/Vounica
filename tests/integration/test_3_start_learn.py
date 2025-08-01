e58ac07fdd9e55ef71fc8246fae32de9731d8a0d
Kod-e <115715642+Kod-e@users.noreply.github.com>
2025-08-01 18:28:00 +0900

    add test

---
+++ tests/integration/test_3_start_learn.py
@@ -0,0 +1,17 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+
+
+@pytest.mark.order(1)
+@pytest.mark.asyncio
+async def test_start_learn(authenticated_async_client: AsyncClient):
+    async with authenticated_async_client.stream(
+        "POST",
+        "/v1/question/agent/chat/stream",
+        params={"user_input": "生成AI関連の英単語を学びたいので、それに合わせたレベルチェックをお願いします"},
+        timeout=None,
+    ) as response:
+        async for chunk in response.aiter_lines():
+            print(chunk)
+    
